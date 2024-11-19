df66aa81214e5a381d0d4a09cdd357ce844027af
mustafa muse <muse3mm@gmail.com>
2024-11-19 20:41:24 +0100

    Add project files.

---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace TodoApplikasjonAPIDelTo.Middleware
+
+{
+
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation($"HTTP {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            _logger.LogInformation($"Response status code: {context.Response.StatusCode}");
+        }
+    }
+
+
+
+}
---
+++ Services/TodoService.cs
@@ -0,0 +1,54 @@
+﻿using TodoApplikasjonAPIDelTo;
+
+namespace TodoApplikasjonAPIDelTo.Services
+{
+    public class TodoService : ITodoService
+    {
+        private static List<Todo> _todos = new List<Todo>();
+        private static int _nextId = 1;
+
+        public async Task<IEnumerable<Todo>> GetTodosAsync()
+        {
+            return await Task.FromResult(_todos);
+        }
+
+        public async Task<Todo> GetTodoByIdAsync(int id)
+        {
+            return await Task.FromResult(_todos.FirstOrDefault(t => t.Id == id));
+        }
+
+        public async Task<Todo> CreateTodoAsync(Todo todo)
+        {
+            todo.Id = _nextId++;
+            _todos.Add(todo);
+            return await Task.FromResult(todo);
+        }
+
+        public async Task<bool> UpdateTodoAsync(int id, Todo todo)
+        {
+            var existingTodo = _todos.FirstOrDefault(t => t.Id == id);
+            if (existingTodo == null)
+            {
+                return await Task.FromResult(false);
+            }
+
+            existingTodo.Title = todo.Title;
+            existingTodo.Description = todo.Description;
+            existingTodo.IsCompleted = todo.IsCompleted;
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteTodoAsync(int id)
+        {
+            var todo = _todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return await Task.FromResult(false);
+            }
+
+            _todos.Remove(todo);
+            return await Task.FromResult(true);
+        }
+    }
+}
